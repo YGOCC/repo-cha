37a001e86a589504ab30203417f3e136117ce100
YGOCC <chadoook1990@gmail.com>
2022-05-07 22:46:40 +0100

    New addition
    Added Buttercherry
    Fixed Cherrypecker

--- script/c19900010.lua
+++ script/c19900010.lua
@@ -1,12 +1,13 @@
 --Geneseed Cherrypecker
 local cid,id=GetID()
 function cid.initial_effect(c)
-  --spsummon
+   --spsummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-		e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCondition(cid.spcon)
 	e1:SetTarget(cid.sptg)
 	e1:SetOperation(cid.spop)
