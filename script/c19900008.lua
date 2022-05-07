1789a3939cdedbc2b1cb16ca9857daf2c61d8cf0
YGOCC <chadoook1990@gmail.com>
2022-05-07 20:31:26 +0100

    Updoots

--- script/c19900008.lua
+++ script/c19900008.lua
@@ -1,105 +1,56 @@
---Cyanusfly of Fiber VINE
-local cid,id=GetID()
+--Geneseed Cherry Manticore
+	local cid,id=GetID()
 Duel.LoadScript("init.lua")
 function cid.initial_effect(c)
 	Evolute.AddOrigEvoluteType(c)
 	c:EnableReviveLimit()
- 	Evolute.AddEvoluteProc(c,nil,6,cid.filter1,cid.filter1,2,99)  
-   --spsummon
+	Evolute.AddEvoluteProc(c,nil,7,cid.filter1,cid.filter2,2,99)  
+	--discard deck & draw
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(cid.spcost)
-	e1:SetTarget(cid.sptg)
-	e1:SetOperation(cid.spop)
+	e1:SetCountLimit(1)
+	e1:SetCost(cid.drcost)
+	e1:SetOperation(cid.drop)
 	c:RegisterEffect(e1)
-   --atk
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetHintTiming(TIMING_END_PHASE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id+10000)
-	e2:SetCost(cid.cost)
-	e2:SetTarget(cid.target)
-	e2:SetOperation(cid.operation)
-	c:RegisterEffect(e2)
+	--attack all
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_ATTACK_ALL)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+end
+function cid.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+		if chk==0 then return e:GetHandler():IsCanRemoveEC(tp,4,REASON_COST) end
+	e:GetHandler():RemoveEC(tp,4,REASON_COST)
 end
 
-
-function cid.filter1(c,ec,tp)
-	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_EFFECT)
-end
-
-
-function cid.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-function cid.filter(c,e,tp)
-	return c:IsSetCard(0x85a) and c:IsType(TYPE_EVOLUTE) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
-end
-function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	  if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cid.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(cid.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,cid.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-end
-function cid.spop(e,tp,eg,ep,ev,re,r,rp)
-local c=e:GetHandler()
-	  local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		if Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)==0 then return end
-	tc:AddEC(3)
-	 local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(2000)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	tc:RegisterEffect(e1)
-tc:AddEC(3)
-	end
-end
-
-function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	 if chk==0 then return e:GetHandler():IsCanRemoveEC(tp,3,REASON_COST) end
-	e:GetHandler():RemoveEC(tp,3,REASON_COST)
-end
-function cid.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and aux.nzatk(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(aux.nzatk,tp,0,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,aux.nzatk,tp,0,LOCATION_MZONE,1,1,nil)
-end
-function cid.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
-		local atk=tc:GetAttack()
-		local e1=Effect.CreateEffect(c)
+function cid.drop(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(0)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetValue(-500)
+		e1:SetRange(LOCATION_MZONE)
+		 e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
-		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_UPDATE_ATTACK)
-			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e2:SetValue(math.ceil(atk/2))
-			c:RegisterEffect(e2)
-		end
+		--indes
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e2:SetValue(1)
+	tc:RegisterEffect(e2)
 	end
+end
+function cid.filter1(c,ec,tp)
+	return c:IsAttribute(ATTRIBUTE_FIRE)
+end
+function cid.filter2(c,ec,tp)
+	return c:IsRace(RACE_PLANT) or c:IsRace(RACE_WYRM)
 end
